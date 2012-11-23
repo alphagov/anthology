72484fe415b83aa1feeeab3237d1b2a1484ffe86
Jordan Hatch <jordan@jordanh.net>
2012-11-23 15:52:59 +0000

    Nickname optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,15 +17,17 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_auth_hash(auth_hash)
     github_id = auth_hash.uid.to_s
+    nickname = auth_hash.info ? auth_hash.info.nickname : nil
 
     current_user = self.where(:github_id => github_id).first
     if current_user
       current_user.update_attributes(
         :name => auth_hash.info.name,
-        :github_login => auth_hash.info.nickname,
+        :github_login => nickname,
       )
     else
-      current_user = self.create!(:github_id => github_id, :name => auth_hash.info.name, :github_login => auth_hash.info.nickname)
+      current_user = self.create!(:github_id => github_id, :name => auth_hash.info.name, :github_login => nickname
+        )
     end
 
     current_user
