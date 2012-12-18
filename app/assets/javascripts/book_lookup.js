433812b121bd94e837117fcc5afa232c48217556
Jordan Hatch <jordan@jordanh.net>
2012-12-18 23:56:44 +0000

    Restructure template markup to contain create button

--- app/assets/javascripts/book_lookup.js
+++ app/assets/javascripts/book_lookup.js
@@ -6,22 +6,25 @@ BookLookup.find_by_isbn = function(isbn, callback) {
 
 BookLookup.find_and_render = function(isbn) {
   if (isbn == "") {
-    $('#book-preview').html('');
+    $('#book-placeholder').html('');
     return;
   }
 
-  $('#book-preview').addClass('loading').html('Searching for book...');
+  $('#book-placeholder').addClass('loading').html('Searching for book...');
   BookLookup.find_by_isbn(isbn, function(data){
     output = Mustache.render($('#book-preview-template').html(), data);
-    $('#book-preview').removeClass('loading').html( output );
+    $('#book-placeholder').removeClass('loading').html( output );
   });
 }
 
 $(document).ready(function() {
   var book_lookup_timeout;
 
-  $('#book_isbn').keyup( function() {
+  $('#new_book input[name=commit]').remove();
+
+  $('#book_isbn').change( function(e) {
     var isbn = $('#book_isbn').val();
+    $('#book-placeholder').addClass('loading').html('Searching for book...');
 
     clearTimeout(book_lookup_timeout);
     book_lookup_timeout = setTimeout(function() {
