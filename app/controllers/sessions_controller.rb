5c7fb82dc187066afaef31f4034bcf31bab26325
Jordan Hatch <jordan@jordanh.net>
2014-08-04 18:01:08 +0100

    Don't verify the authenticity token on session create
    This causes issues with the developer omniauth strategy, and probably 
    isn't the right behaviour when a service redirects back to the app.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :authenticate!
+  skip_before_filter :verify_authenticity_token, only: :create
 
   def new
     # new.html.erb
