660fdcf3573658c683d21c307bc622d6c18f99a3
Jordan Hatch <jordan@jordanh.net>
2014-08-04 19:06:37 +0100

    Change protect_from_forgery to raise an exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
 
   include ApplicationHelper
+  protect_from_forgery with: :exception
 
   before_filter :authenticate!
 
