b846fe2199aab2387c770f47b66090a1299058ba
Jordan Hatch <jordan@jordanh.net>
2014-08-04 19:19:01 +0100

    Make the ApplicationHelper module methods private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-
-  include ApplicationHelper
   protect_from_forgery with: :exception
 
   before_filter :authenticate!
 
-  private
-    def not_found
-      raise ActionController::RoutingError.new('Not Found')
-    end
+private
+  include ApplicationHelper
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end
