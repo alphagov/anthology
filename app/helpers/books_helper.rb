1dca1c1cf71663e3b31cdf30867b7ea5d4037bab
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2016-10-20 11:11:56 +0100

    Fix book images
    Book images from Google are currenly broken since the subdomain has changed. This commit fixes the subdomain so that the images show up again. It also adds the `pry-byebug` gem for development and testing.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -4,7 +4,7 @@ module BooksHelper
     zoom = cover_sizes[size]
 
     if book.google_id
-      image_tag "http://bks0.books.google.co.uk/books?id=#{book.google_id}&printsec=frontcover&img=1&zoom=#{zoom}&edge=none&source=gbs_api", :alt => "#{book.title} by #{book.author}"
+      image_tag "http://books.google.co.uk/books?id=#{book.google_id}&printsec=frontcover&img=1&zoom=#{zoom}&edge=none&source=gbs_api", :alt => "#{book.title} by #{book.author}"
     else
       content_tag :div, :class => "placeholder_book" do
         concat(book.title)
@@ -16,7 +16,7 @@ module BooksHelper
   def cover_urls(book, size = "S")
     response = { }
 
-    response[:google] = "http://bks0.books.google.co.uk/books?id=#{book[:google_id]}&printsec=frontcover&img=1&zoom=#{cover_sizes[size]}&edge=none&source=gbs_api" if book[:google_id]
+    response[:google] = "http://books.google.co.uk/books?id=#{book[:google_id]}&printsec=frontcover&img=1&zoom=#{cover_sizes[size]}&edge=none&source=gbs_api" if book[:google_id]
     response[:openlibrary] = "http://covers.openlibrary.org/b/olid/#{book[:openlibrary_id]}-M.jpg" if book[:openlibrary_id]
 
     response
