b9d20bd29b49132a2f82b5899575b01e421bb150
Issy Long <me@issyl0.co.uk>
2020-05-06 22:49:50 +0100

    Clean up old `new_framework_defaults` initializers
    - Now we're using Rails 6, and all its defaults (except one!), we don't
      need these files any more.
    - For the single default we're not using - because I couldn't
      get my head around it quite yet - set it in `config/application.rb`.
    - This is the `config.active_record.belongs_to_required_by_default`,
      explained [in the Rails ActiveRecord docs](https://edgeguides.rubyonrails.org/configuring.html#configuring-active-record)as:
    
      > config.active_record.belongs_to_required_by_default is a boolean
      > value and controls whether a record fails validation if belongs_to
      > association is not present
    
    - If we set this to true, the majority of our tests error with the following:
    
    ```
    Error:
    StartPageTest::as a signed in user#test_0006_display recent loans from the library:
    ActiveRecord::RecordInvalid: Validation failed: Created by must exist
        test/integration/start_page_test.rb:66:in `block (2 levels) in <class:StartPageTest>'
    ```

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Books
 
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.active_record.belongs_to_required_by_default = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
