24b8cdf3abbcbb9a1f9f8432d1551f888a661d2a
Issy Long <me@issyl0.co.uk>
2020-05-08 18:51:26 +0100

    CopiesController: Enable type checking

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -1,4 +1,4 @@
-# typed: false
+# typed: true
 class CopiesController < ApplicationController
   def show
     @copy = resource
@@ -12,7 +12,7 @@ class CopiesController < ApplicationController
 
   def edit
     @copy = Copy.find_by(book_reference: params[:id])
-    @book = @copy.book
+    @book = T.must(@copy).book
   end
 
   def lookup
@@ -43,7 +43,7 @@ class CopiesController < ApplicationController
 
   def update
     @copy = Copy.find_by(book_reference: params[:id])
-    if @copy.update(params.require(:copy).permit(:shelf_id))
+    if T.must(@copy).update(params.require(:copy).permit(:shelf_id))
       flash[:notice] = "Shelf updated"
       redirect_to copy_path(@copy)
     else
