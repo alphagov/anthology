b36e4aa741707164028d49c10d4c570d2fa7dae8
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2021-04-01 19:07:49 +0100

    Increase test coverage of the RootController
    By adding a test for the library CSV download, which was not being
    tested before.

--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -37,4 +37,21 @@ describe RootController do
       assert_template "start"
     end
   end
+
+  context "the library csv page" do
+    should "generates a CSV file with a row for each book" do
+      FactoryBot.create(:book, id: 1, title: "Book 1", author: "A. One", isbn: "1")
+      FactoryBot.create(:book, id: 2, title: "Book 2", author: "A. Two", isbn: "2")
+
+      get :library_csv
+
+      assert_response :success
+      assert_equal "text/csv", @response.media_type
+
+      csv_data = "id,title,author,isbn,number_of_copies,on_loan_copies\n" \
+                  "1,Book 1,A. One,1,1,0/1\n2,Book 2,A. Two,2,1,0/1\n"
+
+      assert_equal csv_data, @response.body
+    end
+  end
 end
