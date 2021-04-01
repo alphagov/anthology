c8ad6c21d72c51cf34abe7ac320b9b6d4374a6c3
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2021-04-01 20:34:31 +0100

    Add test coverage for Sessions controller
    There were a few code paths that were not tested, so this adds tests for
    those.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,22 @@
 require "test_helper"
 
 describe SessionsController do
-  describe "POST create" do
-    let(:auth_hash) do
-      { "uid" => "12345" }
-    end
-    let(:user) { create(:user) }
+  let(:auth_hash) do
+    { "uid" => "12345" }
+  end
+  let(:user) { create(:user) }
 
-    setup do
-      stub_omniauth_request_auth_hash(auth_hash)
+  setup do
+    stub_omniauth_request_auth_hash(auth_hash)
+  end
+
+  describe "POST create" do
+    it "redirects with an error when the auth hash is empty" do
+      stub_omniauth_request_auth_hash({})
+      post :create, params: { provider: :google }
+
+      assert_redirected_to new_session_path
+      assert_equal "There was a problem signing you in.", @controller.flash[:alert]
     end
 
     it "signs in a user with the provided auth hash" do
@@ -22,6 +30,17 @@ describe SessionsController do
       assert_redirected_to root_path
     end
 
+    it "redirects with an error if the user cannot be found" do
+      User.expects(:find_or_create_from_auth_hash!)
+          .with(auth_hash)
+          .returns(nil)
+
+      post :create, params: { provider: :google }
+
+      assert_nil @controller.session[:user_id]
+      assert_redirected_to new_session_path
+    end
+
     it "redirects with error when creating a user fails" do
       User.expects(:find_or_create_from_auth_hash!)
           .with(auth_hash)
@@ -32,6 +51,21 @@ describe SessionsController do
       assert_redirected_to new_session_path
       assert_match(/Could not sign you in/, @controller.flash[:alert])
       assert_match(/something bad$/, @controller.flash[:alert])
+    end
+  end
+
+  describe "GET sign_out" do
+    it "clears the session and redirects" do
+      User.expects(:find_or_create_from_auth_hash!)
+          .with(auth_hash)
+          .returns(user)
+
+      post :create, params: { provider: :google }
+      assert_equal user.id, @controller.session[:user_id]
+
+      get :sign_out
+      assert_nil @controller.session[:user_id]
+      assert_redirected_to new_session_path
     end
   end
 end
