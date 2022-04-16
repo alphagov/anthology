eb312f3b17677171ae2981f82af20a27a7fe8c2c
Katie Smith <klssmith@users.noreply.github.com>
2022-04-16 12:52:35 +0100

    Upgrade Rails from 6.1.4.7 to 7.0.2.3
    This is the result of upgrading the Rails gem and running the `rails
    app:update` task. A few minor changes were needed to get things
    working, and not all of the new settings in `new_framework_defaults_7_0.rb`
    have been applied yet - the remaining ones will be checked or changed
    later.

--- db/schema.rb
+++ db/schema.rb
@@ -10,69 +10,100 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_180637) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_151215) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "isbn"
-    t.string "author"
-    t.string "google_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "created_by_id"
-    t.string "openlibrary_id"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "copies", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "books", id: :serial, force: :cascade do |t|
+    t.string "title", limit: 255
+    t.string "isbn", limit: 255
+    t.string "author", limit: 255
+    t.string "google_id", limit: 255
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "created_by_id"
+    t.string "openlibrary_id", limit: 255
+  end
+
+  create_table "copies", id: :serial, force: :cascade do |t|
     t.integer "book_id"
     t.integer "book_reference", null: false
     t.boolean "on_loan", default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "missing", default: false
     t.integer "shelf_id"
   end
 
-  create_table "loans", force: :cascade do |t|
+  create_table "loans", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.integer "copy_id"
-    t.string "state", default: "on_loan"
-    t.datetime "loan_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "return_date"
+    t.string "state", limit: 255, default: "on_loan"
+    t.datetime "loan_date", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "return_date", precision: nil
     t.integer "returned_by_id"
     t.integer "returned_to_shelf_id"
   end
 
-  create_table "shelves", force: :cascade do |t|
+  create_table "shelves", id: :serial, force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "email"
-    t.string "provider"
-    t.string "provider_uid"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "email", limit: 255
+    t.string "provider", limit: 255
+    t.string "provider_uid", limit: 255
     t.text "image_url"
   end
 
-  create_table "versions", force: :cascade do |t|
-    t.string "item_type", null: false
+  create_table "versions", id: :serial, force: :cascade do |t|
+    t.string "item_type", limit: 255, null: false
     t.integer "item_id", null: false
-    t.string "event", null: false
-    t.string "whodunnit"
+    t.string "event", limit: 255, null: false
+    t.string "whodunnit", limit: 255
     t.text "object"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
