ae0f83a9afd44cbf898be83dd41f4807032bf96e
Katie Smith <klssmith@users.noreply.github.com>
2022-06-12 21:49:36 +0100

    Write SessionController tests in a functional test format

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,52 +1,63 @@
-require "test_helper"
+require "integration_test_helper"
 
-describe SessionsController do
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   let(:auth_hash) do
     { "uid" => "12345" }
   end
   let(:user) { create(:user) }
 
-  setup do
-    stub_omniauth_request_auth_hash(auth_hash)
-  end
-
   describe "POST create" do
-    it "redirects with an error when the auth hash is empty" do
-      stub_omniauth_request_auth_hash({})
-      post :create, params: { provider: :google }
+    it "redirects with an error if the auth hash is empty" do
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({})
+
+      post "/auth/google"
+      follow_redirect!
 
       assert_redirected_to new_session_path
       assert_equal "There was a problem signing you in.", @controller.flash[:alert]
     end
 
     it "signs in a user with the provided auth hash" do
-      User.expects(:find_or_create_from_auth_hash!)
-          .with(auth_hash)
-          .returns(user)
+      auth_hash = {
+        provider: "google",
+        uid: "12345",
+        info: {
+          name: user.name,
+          email: user.email,
+        },
+      }
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(auth_hash)
 
-      post :create, params: { provider: :google }
+      post "/auth/google"
+      follow_redirect!
 
       assert_equal user.id, @controller.session[:user_id]
       assert_redirected_to root_path
     end
 
     it "redirects with an error if the user cannot be found" do
-      User.expects(:find_or_create_from_auth_hash!)
-          .with(auth_hash)
-          .returns(nil)
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(auth_hash)
 
-      post :create, params: { provider: :google }
+      User.expects(:find_or_create_from_auth_hash!)
+        .with(auth_hash)
+        .returns(nil)
+
+      post "/auth/google"
+      follow_redirect!
 
       assert_nil @controller.session[:user_id]
       assert_redirected_to new_session_path
     end
 
     it "redirects with error when creating a user fails" do
-      User.expects(:find_or_create_from_auth_hash!)
-          .with(auth_hash)
-          .raises(User::CreationFailure.new("something bad"))
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(auth_hash)
 
-      post :create, params: { provider: :google }
+      User.expects(:find_or_create_from_auth_hash!)
+        .with(auth_hash)
+        .raises(User::CreationFailure.new("something bad"))
+
+      post "/auth/google"
+      follow_redirect!
 
       assert_redirected_to new_session_path
       assert_match(/Could not sign you in/, @controller.flash[:alert])
@@ -56,14 +67,22 @@ describe SessionsController do
 
   describe "GET sign_out" do
     it "clears the session and redirects" do
-      User.expects(:find_or_create_from_auth_hash!)
-          .with(auth_hash)
-          .returns(user)
+      auth_hash = {
+        provider: "google",
+        uid: "12345",
+        info: {
+          name: user.name,
+          email: user.email,
+        },
+      }
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(auth_hash)
 
-      post :create, params: { provider: :google }
+      post "/auth/google"
+      follow_redirect!
+
       assert_equal user.id, @controller.session[:user_id]
 
-      get :sign_out
+      get sign_out_path
       assert_nil @controller.session[:user_id]
       assert_redirected_to new_session_path
     end
--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -9,5 +9,9 @@ class ActionDispatch::IntegrationTest
 
   teardown do
     Capybara.use_default_driver
+
+    # The omniauth auth hash gets overwritten in some tests, so we reset it to "the standard one"
+    # at the end of each test
+    prepare_omniauth_for_testing
   end
 end
