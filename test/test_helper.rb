3e73c1df6f7611cc33790ec92a5132ea3e1ab23d
Katie Smith <klssmith@users.noreply.github.com>
2022-07-29 08:47:40 +0100

    Remove webmock gem as test dependency
    This is not being used, so has been removed. The openlibrary and
    googlebooks gems do make external requests but they return the results
    in a class which we then mock using mocha.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,14 +2,11 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 require "rails/test_help"
 require "mocha/minitest"
-require "webmock"
 
 Dir[Rails.root.join("test/support/*.rb")].sort.each { |f| require f }
 
 DatabaseCleaner.strategy = :transaction
 DatabaseCleaner.clean
-
-WebMock.disable_net_connect!
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
